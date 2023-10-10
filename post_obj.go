d0ead344e06afd2492f37d7d46a093df377a1187
Regent <1534997482@qq.com>
2023-10-10 11:39:00 +0800

    fix post bugs and support more fields

--- post_obj.go
+++ post_obj.go
@@ -4,53 +4,27 @@ import (
 	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
-	"encoding/base64"
-	"fmt"
+	"encoding/hex"
 	"io"
 	"mime/multipart"
 	"net/http"
 	"os"
 	"strings"
-	"time"
 )
 
-// PostObject the expirationTime should use a format similar to "2022-06-04T00:00:00Z"
-func (bc *BoxClient) PostObject(bucketName, objKey, expirationTime, filePath string) (*http.Response, error) {
+// PostObject use form to upload object, fields should include all the necessary and optional fields except for x-amz-signature
+func (bc *BoxClient) PostObject(bucketName, objKey, date, encodedPolicy, region, filePath string, fields map[string]string) (*http.Response, error) {
 	v, err := bc.svc.Config.Credentials.Get()
 	if err != nil {
 		return nil, err
 	}
-	currentTime := time.Now()
-	currentDate := currentTime.Format("20060102")
-	xAmzDate := currentTime.Format("20060102T150405Z")
-	credential := fmt.Sprintf("%s/%s/%s/s3/aws4_request", v.AccessKeyID, currentDate, "region")
-
-	algorithm := "AWS4-HMAC-SHA256"
-	policy := fmt.Sprintf(`{"expiration": "%s",
-		"conditions": [
-			{"bucket": "%s" },
-			{"key": "%s"},
-			{"x-amz-algorithm":"%s"},
-			{"x-amz-credential":"%s"},
-			{"x-amz-date":"%s"}
-		]}`, expirationTime, bucketName, objKey, algorithm, credential, xAmzDate)
-
-	encodePolicy := base64.StdEncoding.EncodeToString([]byte(policy))
-	signature := bc.getSignatureKey(v.SecretAccessKey, currentDate, "region", "s3", encodePolicy)
-
-	field := map[string]string{
-		"key":              objKey,
-		"x-amz-algorithm":  algorithm,
-		"x-amz-credential": credential,
-		"x-amz-date":       xAmzDate,
-		"Policy":           encodePolicy,
-		"X-Amz-Signature":  signature,
-	}
+	signature := bc.getSignatureKey(v.SecretAccessKey, date, region, "s3", encodedPolicy)
+	fields["x-amz-signature"] = signature
 
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
 
-	for key, value := range field {
+	for key, value := range fields {
 		err = writer.WriteField(key, value)
 		if err != nil {
 			return nil, err
@@ -94,11 +68,11 @@ func (bc *BoxClient) calculateHMACSHA256(key, data []byte) []byte {
 	return h.Sum(nil)
 }
 
-func (bc *BoxClient) getSignatureKey(secretKey, dateStamp, region, service, encodePolicy string) string {
+func (bc *BoxClient) getSignatureKey(secretKey, dateStamp, region, service, encodedPolicy string) string {
 	dateKey := bc.calculateHMACSHA256([]byte("AWS4"+secretKey), []byte(dateStamp))
 	regionKey := bc.calculateHMACSHA256(dateKey, []byte(region))
 	serviceKey := bc.calculateHMACSHA256(regionKey, []byte(service))
 	signingKey := bc.calculateHMACSHA256(serviceKey, []byte("aws4_request"))
-	signatureBytes := bc.calculateHMACSHA256(signingKey, []byte(encodePolicy))
-	return string(signatureBytes)
+	signatureBytes := bc.calculateHMACSHA256(signingKey, []byte(encodedPolicy))
+	return hex.EncodeToString(signatureBytes)
 }
--- post_obj_test.go
+++ post_obj_test.go
@@ -1,20 +1,34 @@
 package s3box
 
 import (
+	"encoding/base64"
+	"fmt"
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
+	"time"
 )
 
 func TestBoxClient_PostObject(t *testing.T) {
 	svc := buildS3Client(t)
 	bc := NewBoxClient(svc)
 
+	v, err := svc.Config.Credentials.Get()
+	if err != nil {
+		return
+	}
+	currentTime := time.Now()
+	currentDate := currentTime.Format("20060102")
+	xAmzDate := currentTime.Format("20060102T150405Z")
+	credential := fmt.Sprintf("%s/%s/%s/s3/aws4_request", v.AccessKeyID, currentDate, *svc.Config.Region)
+	algorithm := "AWS4-HMAC-SHA256"
+
 	Convey("TestBoxClient_PostObject", t, func() {
 		type args struct {
-			bucket         string
-			objKey         string
-			expirationTime string
-			filePath       string
+			bucket     string
+			objKey     string
+			filePath   string
+			fields     map[string]string
+			expiration string
 		}
 		tests := []struct {
 			name    string
@@ -22,8 +36,8 @@ func TestBoxClient_PostObject(t *testing.T) {
 			want    int
 			wantErr bool
 		}{
-			{"RemoveCaps should success",
-				args{"bkt", "abc", "2023-08-04T00:00:00Z", "./abc.txt"},
+			{"PostObject should success",
+				args{"test", "test-post.txt", "D:/test.txt", map[string]string{"x-amz-meta-test": "test-val"}, "2023-11-04T00:00:00Z"},
 				204,
 				false,
 			},
@@ -31,7 +45,21 @@ func TestBoxClient_PostObject(t *testing.T) {
 
 		for _, tt := range tests {
 			Convey(tt.name, func() {
-				got, err := bc.PostObject(tt.args.bucket, tt.args.objKey, tt.args.expirationTime, tt.args.filePath)
+				tt.args.fields["key"] = tt.args.objKey
+				tt.args.fields["x-amz-algorithm"] = algorithm
+				tt.args.fields["x-amz-credential"] = credential
+				tt.args.fields["x-amz-date"] = xAmzDate
+
+				//fields in conditions and form need to correspond one by one except for bucket
+				policy := fmt.Sprintf(`{"expiration": "%s", "conditions": [{"bucket": "%s"}`, tt.args.expiration, tt.args.bucket)
+				for k, s := range tt.args.fields {
+					policy = fmt.Sprintf(`%s, {"%s": "%s"}`, policy, k, s)
+				}
+				policy += "]}"
+				encodedPolicy := base64.StdEncoding.EncodeToString([]byte(policy))
+				tt.args.fields["policy"] = encodedPolicy
+
+				got, err := bc.PostObject(tt.args.bucket, tt.args.objKey, currentDate, encodedPolicy, *svc.Config.Region, tt.args.filePath, tt.args.fields)
 				So(got, ShouldNotBeNil)
 				So(got.StatusCode, ShouldEqual, tt.want)
 				So(err, ShouldBeNil)
